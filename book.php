11347afc2656fa9755285bd28b8301ce81d1a61e
SaurabhT91 <saurabhtidgam1991@gmail.com>
2024-02-25 15:10:03 +0530

    Create book.php

---
+++ book.php
@@ -0,0 +1,35 @@
+<?php
+
+class Book
+{
+    public $title;
+    public $author;
+    public $price;
+
+    public function __construct(string $title, string $author, int $price){
+
+        $this->title = $title;
+        $this->author = $author;
+        $this->price = $price;
+    }
+
+    public function getTitle(): string 
+    {
+        return $this->title;
+    }
+
+
+    public function getAuthor(): string
+    {
+        return $this->title;
+    }
+
+
+    public function getPrice(): string
+    {
+        return $this->title;
+    }
+
+}
+
+?>
